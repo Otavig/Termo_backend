279e3095779e5e545730f3fc1176d7f931e116f7
Otávio Garcia <125999228+Otavig@users.noreply.github.com>
2024-12-18 20:27:40 -0300

    Atualização 18_12
    - Sortear palavra aleatoriamente
    - Verificar palavra

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Vercel!');
+// Função para ler as palavras do arquivo
+function obterPalavrasDoArquivo(caminhoArquivo) {
+  try {
+    const dados = fs.readFileSync(caminhoArquivo, "utf-8"); // Lê o conteúdo do arquivo
+    const palavras = dados.split(/\s+/); // Divide o conteúdo em palavras
+    return palavras;
+  } catch (erro) {
+    console.error("Erro ao ler o arquivo:", erro.message);
+    return [];
+  }
+}
+
+// Caminho do arquivo com as palavras
+const caminhoArquivoSorteio = "src/palavras_sorteio.txt";
+
+// Rota que sorteia uma palavra aleatória
+app.get("/aleatorio", (req, res) => {
+  const palavras = obterPalavrasDoArquivo(caminhoArquivoSorteio);
+  if (palavras.length === 0) {
+    return res.status(500).send("Erro ao carregar as palavras do arquivo");
+  }
+
+  const palavraAleatoria =
+    palavras[Math.floor(Math.random() * palavras.length)];
+  res.send(palavraAleatoria);
+});
+
+// Rota que verifica se a palavra existe no arquivo
+app.get("/verificar", (req, res) => {
+  const palavra = req.query.palavra;
+  if (!palavra) {
+    return res.status(400).send("A palavra não foi fornecida");
+  }
+
+  const palavras = obterPalavrasDoArquivo(caminhoArquivoSorteio);
+  if (palavras.length === 0) {
+    return res.status(500).send("Erro ao carregar as palavras do arquivo");
+  }
+
+  const palavraExiste = palavras.includes(palavra);
+  res.send(palavraExiste.toString()); // Retonar true or false
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
